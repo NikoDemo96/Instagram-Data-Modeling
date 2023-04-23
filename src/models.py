27a177ccef39dfbd69f1d366f399372b5840952c
Christian Aranguiz <chnicolas.aranguiz@gmail.com>
2023-04-23 18:51:10 -0400

    First version of IG Data Model

--- src/models.py
+++ src/models.py
@@ -7,23 +7,68 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key = True)
+    username = Column(String(30), nullable = False, unique = True)
+    password = Column(String(30), nullable = False)
+    email= Column(String(30), nullable = False, unique = True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    post = relationship('post') 
+    colection = relationship('colection')
+    comments = relationship('comments')
+    direct_messages = relationship('direct_Messages')
+
+
+class Post(Base):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key = True)
+
+    picture = Column(String(30), nullable = False)
+    post_description = Column(String(30), nullable = False)
+    likes = Column(Integer(15), nullable = False)
+
+    user_id = Column(Integer, ForeignKey('user.id'), nullable = False)
+    user = relationship(User)
+
+    comments = relationship('comments')
+
+
+class Colection(Base):
+    __tablename__ = "colection"
+    id = Column(Integer, primary_key = True)
+    colection_name = Column(String(30), nullable = False)
+
+    post_id = Column(Integer, ForeignKey('planets.id'), nullable = False)
+    post = relationship(Post)
+
+    user_id = Column(Integer, ForeignKey('user.id'), nullable = False)
+    user = relationship(User)
+
+class Comments(Base):
+    __tablename__ = "comments"
+    id = Column(Integer, primary_key = True)
+    comment_text = Column(String(30), nullable = False)
+
+    post_id = Column(Integer, ForeignKey('planets.id'), nullable = False)
+    post = relationship(Post)
+
+    user_id = Column(Integer, ForeignKey('user.id'), nullable = False)
+    user = relationship(User)
+
+class Direct_Messages(Base):
+    __tablename__ = "direct_messages"
+    id = Column(Integer, primary_key = True)
+    message_content = Column(String(300), nullable = False)
+
+    post_id = Column(Integer, ForeignKey('planets.id'), nullable = True) #Es True porque un mensaje directo no tiene que tener un post
+    post = relationship(Post)
+
+    user_id = Column(Integer, ForeignKey('user.id'), nullable = False) #Es false porque un mensaje directo tiene que tener si o si un destinatario
+    user = relationship(User)
+
+
+
 
     def to_dict(self):
         return {}
